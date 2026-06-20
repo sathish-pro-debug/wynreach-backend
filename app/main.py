219794f4d311a77923ef81840b961a516b4d8174
sathishwynsync <sathish.wynsync@gmail.com>
2026-06-21 00:19:36 +0530

    wynreach Deployment testing done

--- app/main.py
+++ app/main.py
@@ -248,6 +248,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://reach.wynsync.tech",
+        "https://wynreach-frontend.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
